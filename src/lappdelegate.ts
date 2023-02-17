ffd12c85f639750932b64495960137ff84867f23
yamato3010 <yama3to10yama3@gmail.com>
2023-02-17 10:38:24 +0900

    mousemoveイベントの取得源を変更

--- src/lappdelegate.ts
+++ src/lappdelegate.ts
@@ -339,7 +339,7 @@ function onMouseMoved(e: MouseEvent): void {
     return;
   }
   const rect = canvas.getBoundingClientRect();
-  document.body.addEventListener('mousemove', ev => {
+  top.document.body.addEventListener('mousemove', ev => {
     const posX: number = ev.clientX - rect.left;
     const posY: number = ev.clientY - rect.top;
     LAppDelegate.getInstance()._view.onTouchesMoved(posX, posY);
